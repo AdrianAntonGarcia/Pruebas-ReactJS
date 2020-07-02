e0ee39481dc127d5bcd5284989e82de0823e73f6
Adrián Antón <adrtler@gmail.com>
2020-07-02 18:06:24 +0200

    Contador realizado

---
+++ src/PrimeraApp.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+// FC Functional components
+// Se pueden poner valores por defecto a las prop
+const PrimeraApp = ({ saludo: saludoProp, subitulo }) => {
+	// const saludo = 'Hola Mundo';
+	// console.log(saludoProp);
+	return (
+		<Fragment>
+			<h1>{saludoProp}</h1>
+
+			{/* Para imprimir objetos 
+			<pre>{JSON.stringify(saludo, null, 3)}</pre> */}
+			<p>{subitulo}</p>
+		</Fragment>
+	);
+};
+
+PrimeraApp.propTypes = {
+	saludo: PropTypes.string.isRequired,
+};
+
+/**
+ * Los valores por defecto de las propiedades también se pueden poner aquí.
+ * Si los ponemos aquí si nos aparecerá el componente en la pestaña de herramientas del
+ * desarrollador del navegador web.
+ */
+PrimeraApp.defaultProps = {
+	subitulo: 'Soy un subtitulo'
+}
+
+export default PrimeraApp;
